a82a9c1122e11affa2b6efde7d85f16b15a89c14
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-04-28 16:40:19 +0200

    Setup mongoose connect ion to retrieve user info

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,36 @@
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const { data: session, status } = useSession();
+  const [userInfo, setUserInfo] = useState();
+  const [userInfoStatus, setUserInfoStatus] = useState('loading');
+
+  function getUserInfo() {
+    if (status === "loading") {
+      return;
+    }
+    fetch("/api/users?id="+session.user.id)
+    .then(response => response.json()
+    .then(data => {
+      setUserInfo(data.user);
+      setUserInfoStatus('loaded');
+    }))
+  }
+
+  useEffect(() => {
+    getUserInfo();
+  }, [status]);
+
+  if(userInfoStatus === 'loading') {
+    return <p>loading</p>
+  }
+
+if(!userInfo?.username) {
+  return <p>NO username!</p>
+}
+
   return (
     <main>
       <h1>Hello World</h1>
